5a19dcd7553c6831cb0b11855594600e8a673203
Kshamav123 <kshamavidya1999@gmail.com>
2021-09-08 18:08:16 +0530

    Extending the parameter

--- TestMax.java
+++ TestMax.java
@@ -2,21 +2,60 @@ package com.testmaxg;
 
 public class TestMax<T extends Comparable<T>>{
 	
-		
-		T firstNo,secondNo,thirdNo;
-		
-		//Constructor
-		public TestMax(T firstNo,T secondNo,T thirdNo) {
+T firstNo,secondNo,thirdNo,fourthNo,fifthNo;
+	
+	
+	public TestMax(T firstNo,T secondNo,T thirdNo) {
+		this.firstNo=firstNo;
+		this.secondNo=secondNo;
+		this.thirdNo=thirdNo;
+	}
+	
+
+	public TestMax(T firstNo,T secondNo,T thirdNo,T fourthNo) {
 			this.firstNo=firstNo;
 			this.secondNo=secondNo;
 			this.thirdNo=thirdNo;
+			this.fourthNo=fourthNo;
 		}
-		
-		/*Generic method using compareTo to find out the maximum of Integers
-		 * @param max is used to assume first is maximum then compare with second and third
-		 * Refactoring: To create Generic Class to take in 3 variables of Generic Type
+	
+	
+	public TestMax(T firstNo,T secondNo,T thirdNo,T fourthNo,T fifthNo) {
+			this.firstNo=firstNo;
+			this.secondNo=secondNo;
+			this.thirdNo=thirdNo;
+			this.fourthNo=fourthNo;
+			
+	}
+	
+	/*Generic method using compareTo to find out the maximum of Integers
+	 * @param max is used to assume first is maximum then compare with second and third
+	 * and a method to print
+	 */
+	 public static <T extends Comparable<T>> T maximumNo(T firstNo,T secondNo,T thirdNo) {
+			T max = firstNo;  
+			if(secondNo.compareTo(max)>0) {
+				max=secondNo;
+			}
+			if(thirdNo.compareTo(max)>0) {
+				max=thirdNo;
+			}
+			printMax(firstNo,secondNo,thirdNo,max);
+			return max;
+			
+		}
+	 
+	 
+	 public static <T> void printMax(T firstNo,T secondNo,T thirdNo,T max)
+		{
+			System.out.printf("Maximum of %s, %s and %s is %s .\n",firstNo,secondNo,thirdNo,max);
+		}
+
+	/*Generic method using compareTo to find out the maximum of Integers
+		 * @param max is used to assume first is maximum then compare with second, third and fourth
+		 * and a method to print
 		 */
-		 public static <T extends Comparable<T>> T maximumNo(T firstNo,T secondNo,T thirdNo) {
+	public static <T extends Comparable<T>> T maximumNo(T firstNo,T secondNo,T thirdNo,T fourthNo) {
 				T max = firstNo;  
 				if(secondNo.compareTo(max)>0) {
 					max=secondNo;
@@ -24,14 +63,47 @@ public class TestMax<T extends Comparable<T>>{
 				if(thirdNo.compareTo(max)>0) {
 					max=thirdNo;
 				}
-				printMax(firstNo,secondNo,thirdNo,max);
+				if(fourthNo.compareTo(max)>0) {
+					max=fourthNo;
+				}
+				printMax(firstNo,secondNo,thirdNo,fourthNo,max);
 				return max;
 			}
-		 
-		 //Method to print the maximum Value
-		 public static <T> void printMax(T firstNo,T secondNo,T thirdNo,T max)
-			{
-				System.out.printf("Maximum of %s, %s and %s is %s .\n",firstNo,secondNo,thirdNo,max);
+	
+	
+	
+		 public static <T> void printMax(T firstNo,T secondNo,T thirdNo,T fourthNo,T max)
+				{
+					System.out.printf("Maximum of %s, %s, %s and %s is %s .\n",firstNo,secondNo,thirdNo,fourthNo,max);
+				}
+	
+	
+	
+	 /*Generic method using compareTo to find out the maximum of Integers
+	 * @param max is used to assume first is maximum then compare with second, third and fourth
+	 * and a method to print
+	 */
+	public static <T extends Comparable<T>> T maximumNo(T firstNo,T secondNo,T thirdNo,T fourthNo,T fifthNo) {
+		T max = firstNo;
+		if(secondNo.compareTo(max)>0) {
+			max=secondNo;
+		}
+		if(thirdNo.compareTo(max)>0) {
+			max=thirdNo;
+		}
+		if(fourthNo.compareTo(max)>0) {
+			max=fourthNo;
+		}
+		if(fifthNo.compareTo(max)>0) {
+			max=fifthNo;
+		}
+		printMax(firstNo,secondNo,thirdNo,fourthNo,fifthNo,max);
+		return max;
 			}
+	
+		public static <T> void printMax(T firstNo,T secondNo,T thirdNo,T fourthNo,T fifthNo,T max)
+				{
+					System.out.printf("Maximum of %s, %s, %s, %s and %s is %s .\n",firstNo,secondNo,thirdNo,fourthNo,fifthNo,max);
+				}
 
 	}
