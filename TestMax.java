2be712d6a7acaa3fc62ce3459d968401f2196b6a
Kshamav123 <kshamavidya1999@gmail.com>
2021-09-08 14:24:04 +0530

    To find the max of float numbers

--- TestMax.java
+++ TestMax.java
@@ -2,15 +2,15 @@ package com.testmaxg;
 
 public class TestMax{
 	
-	public static Integer firstNo,secondNo,thirdNo;
-	TestMax(Integer firstNo,Integer secondNo,Integer thirdNo ){
+	public static Float firstNo,secondNo,thirdNo;
+	TestMax(){
 		this.firstNo=firstNo;
 		this.secondNo=secondNo;
 		this.thirdNo=thirdNo;
 	}
 	
-	public static void maxNo() {
-		Integer max= firstNo;
+	public  float maxNo(Float firstNo, Float secondNo, Float thirdNo) {
+		Float max= firstNo;
 		if(secondNo.compareTo(max)>0) {
 			
 			max=secondNo;
@@ -19,8 +19,10 @@ public class TestMax{
 			max=thirdNo;
 		}
 		
+		
 		System.out.println("Maximum of "+firstNo+","+secondNo+" and "+thirdNo+" is " +max);
 		
+		return max;
 	}
 	
 
