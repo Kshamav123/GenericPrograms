28717111a95dfdf69bcaed3ac993627d7a987aa1
Kshamav123 <kshamavidya1999@gmail.com>
2021-09-08 14:29:53 +0530

    To find the max of float numbers

--- MaxMain.java
+++ MaxMain.java
@@ -2,16 +2,13 @@ package com.testmaxg;
 
 public class MaxMain {
 	public static void main(String args[]) {
-		
-		
-		TestMax obj=new TestMax();
-		obj.maxNo(40.6f, 40.6f, 36.6f);
-		
-		TestMax obj2=new TestMax();
-		obj.maxNo(34.6f,54.7f,23.7f);
-	
-		TestMax obj3=new TestMax();
-		obj3.maxNo(45.6f,63.6f,23.6f);
+
+		TestMax obj = new TestMax();
+		obj.maxNo(40.6f, 20.6f, 36.6f);
+		TestMax obj2 = new TestMax();
+		obj.maxNo(34.6f, 54.7f, 23.7f);
+		TestMax obj3 = new TestMax();
+		obj3.maxNo(15.6f, 63.6f, 93.6f);
 	}
 
 }
